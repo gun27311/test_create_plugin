3ce4837b12b740fbbd2cf37da9f0bdbcf986a15e
kanthakran kalawong <kanthakrankalawong@kanthakrans-MacBook-Air.local>
2019-06-15 14:08:28 +0700

    init plugin

---
+++ includes/class-v00-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       -
+ * @since      1.0.0
+ *
+ * @package    V00
+ * @subpackage V00/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    V00
+ * @subpackage V00/includes
+ * @author     kanthakran <gun_27311@hotmail.com>
+ */
+class V00_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-v00-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       -
+ * @since      1.0.0
+ *
+ * @package    V00
+ * @subpackage V00/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    V00
+ * @subpackage V00/includes
+ * @author     kanthakran <gun_27311@hotmail.com>
+ */
+class V00_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
